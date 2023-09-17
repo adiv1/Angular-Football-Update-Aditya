eca6676f6a319eb14c44a1eec5879ba05751da41
adiv1 <30501822+adiv1@users.noreply.github.com>
2023-09-18 04:14:59 +0530

    1 file modified, 7 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { LeagueDashboardComponent } from './components/league-dashboard/league-dashboard.component';
 import { GameDetailsComponent } from './components/game-details/game-details.component';
 import { GridrenderComponent } from './components/gridrender/gridrender.component';
@@ -34,8 +31,6 @@ import { MatTabsModule } from '@angular/material/tabs';
   ],
   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent,
     LeagueDashboardComponent,
     GameDetailsComponent,
     GridrenderComponent,
