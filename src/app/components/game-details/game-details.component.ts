a84c68b468bfc3b4d1ad3c1dc3112273205b69df
adiv1 <30501822+adiv1@users.noreply.github.com>
2023-09-18 04:11:07 +0530

    1 file modified

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
+import { gamedetailsGridColumns } from './game-details';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FootballgameService } from 'src/app/services/footballgame.service';
 
 @Component({
   selector: 'app-game-details',
   templateUrl: './game-details.component.html',
-  styleUrls: ['./game-details.component.css']
+  styleUrls: ['./game-details.component.css'],
 })
 export class GameDetailsComponent {
+  public columnDefs: ColDef[] = gamedetailsGridColumns;
+  public rowData = [];
+  public teamId: number | undefined;
+  constructor(
+    public route: ActivatedRoute,
+    public router: Router,
+    public footBallAPI: FootballgameService
+  ) {}
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.teamId = +params['teamId'];
+      this.getTeamFixtures(this.teamId);
+    });
+  }
+  getTeamFixtures(teamId: Number) {
+    this.footBallAPI.getTopList(teamId).subscribe((data: any) => {
+      this.rowData = data.response;
+      // this.country = data.response[0].league.id;
+      console.log(data);
+    });
+  }
 }
